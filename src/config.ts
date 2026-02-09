a6ea1c569a7c64cc30c4250a7f37004c5c491dfb
sun2ot <84566015+sun2ot@users.noreply.github.com>
2026-02-09 09:17:17 +0800

    update font

--- src/config.ts
+++ src/config.ts
@@ -197,9 +197,9 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "ZenMaruGothic-Medium",
+			fontFamily: "LXGW WenKai",
 			fontWeight: "400",
-			localFonts: ["ZenMaruGothic-Medium.ttf"],
+			localFonts: ["LXGWWenKai-Regular.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
