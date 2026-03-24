51f82f219ba1e84e89c5803fd53639a32d2005b7
sun2ot <84566015+sun2ot@users.noreply.github.com>
2026-03-24 13:12:38 +0800

    fix settings

--- src/config.ts
+++ src/config.ts
@@ -195,11 +195,11 @@ export const siteConfig: SiteConfig = {
 	},
 	toc: {
 		enable: true, // 总开关，启用目录功能
-		mobileTop: true, // 手机端顶部 TOC 按钮
-		desktopSidebar: true, // 电脑端右侧边栏 TOC
+		mobileTop: false, // 手机端顶部 TOC 按钮
+		desktopSidebar: false, // 电脑端右侧边栏 TOC
 		floating: true, // 悬浮 TOC 按钮
 		depth: 2, // 目录深度，1-6，1 表示只显示 h1 标题，2 表示显示 h1 和 h2 标题，依此类推
-		useJapaneseBadge: true, // 使用日语假名标记（あいうえお...）代替数字，开启后会将 1、2、3... 改为 あ、い、う...
+		useJapaneseBadge: false, // 使用日语假名标记（あいうえお...）代替数字，开启后会将 1、2、3... 改为 あ、い、う...
 	},
 	showCoverInContent: true, // 在文章内容页显示文章封面
 	generateOgImages: false, // 启用生成OpenGraph图片功能,注意开启后要渲染很长时间，不建议本地调试的时候开启
