6993780b1f54fa383206f1e68b50fd2b7e02b194
sun2ot <84566015+sun2ot@users.noreply.github.com>
2026-03-25 19:38:16 +0800

    remove some images

--- src/config.ts
+++ src/config.ts
@@ -251,11 +251,8 @@ export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 		], // 桌面横幅图片
 		mobile: [
 			"/assets/mobile-banner/1.webp",
-			"/assets/mobile-banner/2.webp",
 			"/assets/mobile-banner/3.webp",
 			"/assets/mobile-banner/4.webp",
-			"/assets/mobile-banner/5.webp",
-			"/assets/mobile-banner/6.webp",
 		], // 移动横幅图片
 	}, // 使用本地横幅图片
 	position: "center", // 壁纸位置，等同于 object-position
